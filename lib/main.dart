e2fb25ceb953c690216be17b4502b1784a818406
Ali Zarezadeh <alyzarezadeh@gmail.com>
2021-01-25 23:10:09 +0330

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() => runApp(ToDo());
 class ToDo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.green),
